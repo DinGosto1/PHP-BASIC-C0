0b12bcad1ef1cce9ed607cab232ae593cf036c40
Din Gosto <din.gosto@student.rocva.nl>
2022-02-10 13:04:07 +0100

    opdracht vebeterd

--- Eindopdracht/bitcoinData.php
+++ Eindopdracht/bitcoinData.php
@@ -18,7 +18,7 @@
 /* 
  * Opdracht 2: Zet hier de $buttons array neer 
  */
-$buttons = array(400, 500, 750, 1000, 1500);
+$buttons = array(400, 500, 750, 1000, 1500, 333);
 
 
 /******************************************* OPDRACHT 2 doe je hierboven *******************************************/
@@ -81,7 +81,7 @@ if (isset($_GET['euro'])) {
      * Schrijf hieronder de code om de gekochte bitcoin aan de array $transactions toe te voegen.
      * Je voegt een bitcoinprijs toe, de prijs in euro's en het aantal gekochte bitcoins.
      */
-    array_push($transaction, [getBitcoinPrice(), $euro, calculateBitcoinAmount()]);
+     array_push($transactions, [getBitcoinPrice(), $euro, calculateBitcoinAmount()]);
 }
 
 
